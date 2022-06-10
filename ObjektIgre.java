be586b6b7425156426997331414585ff4d4fc745
Wiks0 <65351417+Wiks0@users.noreply.github.com>
2022-06-11 01:59:40 +0200

    Create ObjektIgre.java

---
+++ ObjektIgre.java
@@ -0,0 +1,74 @@
+package proj.sprite;
+
+import java.awt.Image;
+
+public class objektIgre {
+
+    private boolean visible;
+    private Image image;
+    private boolean dying;
+
+    int x;
+    int y;
+    int dx;
+
+    public objektIgre() {
+
+        visible = true;
+    }
+
+    public void die() {
+
+        visible = false;
+    }
+
+    public boolean isVisible() {
+
+        return visible;
+    }
+
+    protected void setVisible(boolean visible) {
+
+        this.visible = visible;
+    }
+
+    public void setImage(Image image) {
+
+        this.image = image;
+    }
+
+    public Image getImage() {
+
+        return image;
+    }
+
+    public void setX(int x) {
+
+        this.x = x;
+    }
+
+    public void setY(int y) {
+
+        this.y = y;
+    }
+
+    public int getY() {
+
+        return y;
+    }
+
+    public int getX() {
+
+        return x;
+    }
+
+    public void setDying(boolean dying) {
+
+        this.dying = dying;
+    }
+
+    public boolean isDying() {
+
+        return this.dying;
+    }
+}
