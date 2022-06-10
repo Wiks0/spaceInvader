3b51eacde2856d756373f6b6735583dc8b390e76
Wiks0 <65351417+Wiks0@users.noreply.github.com>
2022-06-11 01:59:12 +0200

    Create nastavitve.java

---
+++ nastavitve.java
@@ -0,0 +1,21 @@
+package proj.sprite;
+
+public interface nastavitve {
+
+    int BOARD_WIDTH = 600;
+    int BOARD_HEIGHT = 700;
+    int GROUND = 600;
+    int BOMB_HEIGHT = 5;
+
+    int ALIEN_HEIGHT = 16;
+    int ALIEN_WIDTH = 16;
+    int ALIEN_INIT_X = 150;
+    int ALIEN_INIT_Y = 5;
+
+    int GO_DOWN = 15;
+    int NUMBER_OF_ALIENS_TO_DESTROY = 45;
+    int CHANCE = 5;
+    int DELAY = 17;
+    int PLAYER_WIDTH = 20;
+    int PLAYER_HEIGHT = 20;
+}
